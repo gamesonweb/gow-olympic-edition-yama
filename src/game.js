64759dbc99ad22bbf8960277dda5c46edff123b0
Kazenoyama <kishan.turpin@gmail.com>
2024-05-28 11:16:37 +0200

    fps rate try

--- src/game.js
+++ src/game.js
@@ -18,7 +18,7 @@ class Game {
 
         // Set the target frame rate to 60 fps
         this.engine.setHardwareScalingLevel(1 / (window.devicePixelRatio || 1));
-        this.targetFPS = 165;
+        this.targetFPS = 144;
         this.frameDuration = 1000 / this.targetFPS;
         this.lastFrameTime = 0;
     }
